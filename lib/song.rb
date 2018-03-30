39869a42968bb4d929fc565b5cccf253d5dff1d0
Kyle Harris <k_harris95@live.com>
2018-03-30 03:57:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,10 @@ class Song
   end
 
   def new_by_filename(filename)
-
+    parse = file.split(" - ")
+    name = parse[0]
+    song_name = parse[1]
+    song = self.new(song_name)
+    song
   end
 end
